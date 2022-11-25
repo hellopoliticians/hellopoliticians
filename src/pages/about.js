05aa33835fa4cf3dc477d288744ce9a7d305deb1
Lluis Agusti Sanes <lluis.agusti@melia.com>
2022-11-25 17:51:38 +0100

    react upgrade

---
+++ src/pages/about.js
@@ -0,0 +1,64 @@
+import React, { useEffect } from "react";
+import styled from "styled-components";
+import { Link, useLoad } from "react-router-dom-animation";
+import { motion } from "framer-motion";
+
+const Body = styled(motion.div)`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  color: #fff;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+`;
+
+const Page = (routerLocation, isAnimation) => {
+  const [isLoad, setLoad] = useLoad();
+
+  useEffect(() => {
+    setLoad(true);
+  }, []);
+
+  return (
+    <Body
+      style={{ background: "#1bbc9b", zIndex: 111 }}
+      initial={{ scale: isLoad ? 1 : 1 }}
+      animate={{ scale: isAnimation ? 1 : isLoad ? 1 : 1 }}
+      exit={{ scale: 1 }}
+      transition={{ duration: 0.3 }}
+    >
+      <div>
+        <div>
+          <Link style={{ textDecoration: "none" }} to="/2" animate="prev">
+            <div
+              style={{
+                color: "#fff",
+                height: "99vh",
+                width: "95vw",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                fontSize: 14,
+                paddingLeft: "2em",
+                paddingRight: "2em",
+                textAlign: "center",
+              }}
+            >
+              <div>
+              <h2>Do you remember climate change?</h2>
+              <br />
+              <p>...hypocrites.</p>
+              <br />
+              <p>November 2022</p>
+              </div>
+            </div>
+          </Link>
+        </div>
+      </div>
+    </Body>
+  );
+};
+
+export default Page;
---
+++ src/pages/home.js
@@ -0,0 +1,65 @@
+import React, { useEffect } from "react";
+import styled from "styled-components";
+import { Link, useLoad } from "react-router-dom-animation";
+import { motion } from "framer-motion";
+
+const Body = styled(motion.div)`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  color: #fff;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+`;
+
+const Page = (routerLocation, isAnimation) => {
+  const [isLoad, setLoad] = useLoad();
+
+  useEffect(() => {
+    setLoad(true);
+  }, []);
+
+  const handleScroll = () => {};
+
+  return (
+    <Body
+      style={{ background: "#1bbc9b", zIndex: 111 }}
+      initial={{ scale: isLoad ? 1 : 1 }}
+      animate={{ scale: isAnimation ? 1 : isLoad ? 1 : 1 }}
+      exit={{ scale: 1 }}
+      transition={{ duration: 0.3 }}
+    >
+      <div>
+        <Link style={{ textDecoration: "none" }} to="/" animate="next">
+          <div
+            style={{
+              color: "#fff",
+              height: "99vh",
+              width: "95vw",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              fontSize: 14,
+              paddingLeft: "2em",
+              paddingRight: "2em",
+              textAlign: "center",
+            }}
+          >
+            <div>
+              <h1>Hello Politicians</h1>
+              <p>We need to build a better world.</p>
+              <p>But you are not up to the task.</p>
+              <p style={{ lineHeight: 2 }}>
+              This project interpellates you constructively.
+              </p>
+            </div>
+          </div>
+        </Link>
+      </div>
+    </Body>
+  );
+};
+
+export default Page;
