49115fc40dadbebf03796ac830c46ab2490ac3c9
Lluis Agusti Sanes <lluis.agusti@melia.com>
2023-01-20 14:27:45 +0100

    but

--- src/pages/about.js
+++ src/pages/about.js
@@ -49,7 +49,7 @@ const Page = (routerLocation, isAnimation) => {
               <div>
               <h1>Hello Politicians</h1>
               <br />
-              <h2>Sorry, there is no money for:</h2>
+              <h2>There is no money for:</h2>
               <h2>Healthcare</h2>
               <h2>Education</h2>
               <h2>Retirement</h2>
